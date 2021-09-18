a5ab0c6b89cf45782bcea2733f613c3f103f96c3
terminatorPG <55758267+prabhu2599@users.noreply.github.com>
2021-09-18 11:31:08 +0530

    Create Java String Tokens.java

---
+++ Java String Tokens.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //String s = scan.nextLine();
+        // Write your code here.
+        String s = scan.nextLine().trim();
+        //System.out.println(s);
+        if (s.length() > 0) {
+          String[] tokens = s.split("[!,?._'@\\s]+");
+          System.out.println(tokens.length);
+          for(String token : tokens) {
+            System.out.println(token);
+          }
+        } else {
+          System.out.println(0);
+        }
+        scan.close();
+    }
+}
+
